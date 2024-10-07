5f4ed08fde8273db673df33a8793b434232767ef
Tarantul4444 <baitukenovtimur@mail.ru>
2024-10-07 12:46:34 +0500

    Multiply

--- Matrix.py
+++ Matrix.py
@@ -8,12 +8,28 @@ class Matrix:
         if self.rowLength != other.rowLength or self.columnLength != other.rowLength:
             raise ValueError("Матрицы должны быть одинакового размера для сложения.")
 
-        result = [[self.matrix[i][j] + other.matrix[i][j] for j in range(self.columnLength)] for i in range(self.rowLength)]
+        result = [[self.matrix[i][j] + other.matrix[i][j] for j in range(self.columnLength)] for i in
+                  range(self.rowLength)]
         return Matrix(result)
 
     def __sub__(self, other):
         if self.rowLength != other.rowLength or self.columnLength != other.rowLength:
             raise ValueError("Матрицы должны быть одинакового размера для вычитания.")
 
-        result = [[self.matrix[i][j] - other.matrix[i][j] for j in range(self.columnLength)] for i in range(self.rowLength)]
+        result = [[self.matrix[i][j] - other.matrix[i][j] for j in range(self.columnLength)] for i in
+                  range(self.rowLength)]
+        return Matrix(result)
+
+    def __mul__(self, other):
+        if self.columnLength != other.rowLength:
+            raise ValueError(
+                "Количество столбцов первой матрицы должно совпадать с количеством строк второй матрицы для умножения.")
+
+        result = [[0 for _ in range(other.columnLength)] for _ in range(self.rowLength)]
+
+        for i in range(self.rowLength):
+            for j in range(other.columnLength):
+                for k in range(self.columnLength):
+                    result[i][j] += self.matrix[i][k] * other.matrix[k][j]
+
         return Matrix(result)
